6bfa51b71bab1f023cf9df50d2656cda5021dabc
Rui Wang <77094591+CptWang@users.noreply.github.com>
2021-12-10 02:32:22 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# Python Application for Vaccine Scheduler
+=======
+# vaccine-scheduler-python
+UW CSE414 Autumn 2021 HW6
+>>>>>>> 41a32ec1cf7f1491b7ad2b958a6e89e78d855827
---
+++ src/main/resources/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Caregivers (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Availabilities (
+    Time date,
+    Username varchar(255) REFERENCES Caregivers,
+    PRIMARY KEY (Time, Username)
+);
+
+CREATE TABLE Vaccines (
+    Name varchar(255),
+    Doses int,
+    PRIMARY KEY (Name)
+);
+
+CREATE TABLE Patients (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Appointments (
+    Id int,
+    Cusername varchar(255) REFERENCES Caregivers,
+    Pusername varchar(255) REFERENCES Patients,
+    Vname varchar(255) REFERENCES Vaccines,
+    Time date,
+    PRIMARY KEY (Id)
+);
